ae8c7a354212cff5c56d135992b5d753ff27664a
kremlin <ian@kremlin.cc>
2014-06-07 03:45:04 -0400

    XML configs will be either compiled in (default) or installed (flagged), rm workarounds

--- hacks/unpunch_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# unsticks our config for hostnamed (which needs to use the system bus)
-# and restarts dbus
-
-rm /etc/dbus-1/system.d/hostnamed-dbus.conf
-/etc/rc.d/dbus_daemon reload >> /dev/null
