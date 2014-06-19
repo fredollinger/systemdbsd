5047f6bfd1a812c46128b0d68bf7b93c21c2e76c
kremlin <ian@kremlin.cc>
2014-06-19 14:59:16 -0500

    regenerate interface types, clean up gen script

--- scripts/gen-gdbus-interfaces.sh
+++ scripts/gen-gdbus-interfaces.sh
@@ -5,12 +5,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd )"
 if [[ -z "$1" ]]; then 
 	echo "syntax: ./gen-dbus-interfaces <interface name>"
 	exit 1
+elif [[ "$1" != "hostnamed" && "$1" != "localed" && "$1" != "timedated" && "$1" != "logind" ]]; then 
+	echo "<interface name> must be 'hostnamed', 'localed', 'timedated', or 'logind'"
+	exit 1
 fi
 
 gdbus-codegen \
 --interface-prefix org.freedesktop. \
 --generate-docbook doc/$1-docbook.xml \
 --generate-c-code  src/interfaces/$1/$1-gen \
---c-namespace SystemdUtils \
 conf/$1-ispect.xml
 
+echo "success"
+exit 0
