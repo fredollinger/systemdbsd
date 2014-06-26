d90aa67d8dbd325da6a1f42a915cfa8724c5cd27
kremlin <ian@kremlin.cc>
2014-06-25 23:18:16 -0500

    more work on xml configuration setting..

--- src/config.c
+++ src/config.c
@@ -17,30 +17,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <gio/gio.h>
+#include "config.h"
 
 static GKeyFile *config;
 static int config_descr;
-
 static gchar *data_dir;
-
-/*static int hostnamed_ispect_xml_descr, hostnamed_dbus_xml_descr;
-static int localed_ispect_xml_descr, localed_dbus_xml_descr;
-static int timedated_ispect_xml_descr, timedated_dbus_xml_descr;
-static int logind_ispect_xml_descr, logind_dbus_xml_descr;
-
-/*TODO depending on builtin xml flag, these should be matched to checksums */    /*
-static gchar **hostnamed_ispect_xml, hostnamed_dbus_xml;
-static gchar **localed_ispect_xml, localed_dbus_xml;
-static gchar **timedated_ispect_xml, timedated_dbus_xml;
-static gchar **logind_ispect_xml, logind_dbus_xml; */
-
-static void set_xml_descriptors(); 
-
-static const gchar *CONFIG_KEYS[] = {
-    "PrettyHostname",
-    "IconName",
-    "ChassisType"
-};
 
 /* NULL if key doesn't exist */
 gchar *get_option(gchar *key, gchar *group) {
@@ -68,20 +49,19 @@ gboolean set_option(gchar *key, gchar *value, gchar *group) {
 /* initial load/check */
 gboolean config_init() {
 
-    if(config)
-        return TRUE; //already init'd
-
     config = g_key_file_new();
     
     const gchar *config_path;
     GStatBuf *config_lstat;
+	int config_lstat_ret;
 
     config_path = "/etc/systemd_compat.conf";
+	config_lstat_ret = g_lstat(config_path, config_lstat);
 
     /* does conf exist? */
-    if(g_lstat(config_path, config_lstat)) {
+    if(config_lstat_ret) {
 
-        /* if not, can we write it */
+        /* if not, can we write to where it should be? */
         if(g_access("/etc/", W_OK)) {
             g_printf("%s\n", "no write permissions for /etc/! exiting..");
             return FALSE;
@@ -128,78 +108,61 @@ gboolean config_init() {
 
 gboolean init_xml() {
 
-	const gchar * const *data_dir_prefix;	
+	const gchar * const *data_dir_prefix;
+	int xml_lstat_ret;
+	GStatBuf *xml_lstat;
 
 	data_dir_prefix = g_get_system_data_dirs();
 	data_dir = g_strconcat(data_dir_prefix[0], "systemd_compat", NULL);
-
-	GStatBuf *xml_lstat;
+	
+	xml_lstat_ret = g_lstat(data_dir, xml_lstat);
 
 	/* does xml dir exist? */
-	if(g_lstat(data_dir, xml_lstat)) {
+	if(xml_lstat_ret) {
 
-		/* if not, can we write it? */
+		/* if not, can we write to where it should be? */
 		if(g_access(data_dir_prefix[0], W_OK)) {
 			g_printf("no write permissions for %s! exiting...\n", data_dir_prefix[0]);
 			return FALSE;
 		}
 
 		g_printf("creating xml data directory %s...\n", data_dir);
-		if(g_mkdir(data_dir, 644)) {
+
+		if(g_mkdir(data_dir, 644) || g_access) {
 			g_printf("failed to create dir %s...\n", data_dir);
 			return FALSE;
 		}
 
-		set_xml_descriptors();
-	}
-	return TRUE; /* kill me! */
-} /* kill me too!*/
-
-//LEFTOFF
-		
-/*		gchar *posix_hostname;
-		posix_hostname = g_malloc(255); 
-
-		gethostname(posix_hostname, 255);
-
-		g_key_file_set_string(config, "hostnamed", "Hostname", posix_hostname);
-		g_key_file_set_string(config, "hostnamed", "PrettyHostname", "");
-		g_key_file_set_string(config, "hostnamed", "IconName", "Computer");	
-		g_key_file_set_string(config, "hostnamed", "ChassisType", "laptop"); //TODO set these correctly
-
-		if(!g_key_file_save_to_file(config, config_path, NULL)) {
-			g_printf("failed to write config to %s!\n", config_path);
-			g_free(posix_hostname);
+		/* read in xml files from conf/ */
+		if(!read_xml_from_installconf()) {
+			g_printf("failed to read xml configs in conf/...\n");
 			return FALSE;
 		}
 
-		g_printf("wrote config to %s\n", config_path);
+		/* write our configs to system data dir */
+		if(!populate_xml_data_dir()) {
+			g_printf("failed to write xml configs to %s...\n", data_dir);
+			return FALSE;
+		}
 
-		g_free(posix_hostname);
+		/* get descriptors from freshly-installed configs */
+		if(!set_xml_descriptors()) {
+			g_printf("failed to fopen xml configs...\n");
+			return FALSE;
+		}
 
 		return TRUE;
-
-	/* it does exist, read it */     /*
+	
+	/* it does exist, read it */
 	} else {
 
-		if(!g_access(config_path, W_OK)) {
-			g_printf("%s\n", "no write permissions for /etc/! exiting..");
+		if(!set_xml_descriptors()) {
+			g_printf("failed to fopen xml configs...\n");
 			return FALSE;
-		} else if(g_key_file_load_from_file(config, config_path, G_KEY_FILE_KEEP_COMMENTS, NULL)) {
-			config_descr = g_open(config_path, O_RDWR, 644);
-			return TRUE;
 		}
 
-		g_printf("could not read config at %s! exiting..", config_path);
-		return FALSE;
+		return TRUE;
 	}
-
-
-	return TRUE;
-} */
-
-static void set_xml_descriptors() {
-	
 }
 
 void clean_config() {
---
+++ src/config.h
@@ -0,0 +1,56 @@
+/* manually increment this if adding new functionality/config files */
+static const guint XML_FILE_COUNT = 8;
+
+
+/* our buffers for xml config file descriptors */
+static int hostnamed_ispect_xml_descr, hostnamed_dbus_xml_descr,
+           localed_ispect_xml_descr, localed_dbus_xml_descr,
+           timedated_ispect_xml_descr, timedated_dbus_xml_descr,
+           logind_ispect_xml_descr, logind_dbus_xml_descr;
+
+static int *xml_descriptors[] = {
+
+	&hostnamed_ispect_xml_descr, &hostnamed_dbus_xml_descr,
+	&localed_ispect_xml_descr, &localed_dbus_xml_descr,
+	&timedated_ispect_xml_descr, &timedated_dbus_xml_descr,
+	&logind_ispect_xml_descr, &logind_dbus_xml_descr
+};
+
+/*our buffers for xml config file contents */
+static gchar **hostnamed_ispect_xml, **hostnamed_dbus_xml,
+			 **localed_ispect_xml, **localed_dbus_xml,
+			 **timedated_ispect_xml, **timedated_dbus_xml,
+			 **logind_ispect_xml, **logind_dbus_xml;
+
+static gchar ***xml_contents[] = {
+
+	&hostnamed_ispect_xml, &hostnamed_dbus_xml,
+	&localed_ispect_xml, &localed_dbus_xml,
+	&timedated_ispect_xml, &timedated_dbus_xml,
+	&logind_ispect_xml, &logind_dbus_xml
+};
+
+#ifndef ALLOW_EDITED_XML
+
+static const gchar *hostnamed_dbus_sum   = "ad7536541463e426388c7562b0e7a693c3b646539b3ae163a7caf6ad95f10e08";
+static const gchar *hostnamed_ispect_sum = "4a41b3c3b0169fb7859ed5a2bde9c8c92f2b67e5e22b6e240cb6f41f6d2e69db";
+static const gchar *localed_dbus_sum     = "fd3a5e13f660ca393d66e518eb5ba1b94e26ad055f7485955ba0070eec4e217c";
+static const gchar *localed_ispect_sum   = "3ef138659ebfd3a8b7bb11d658ed80867493e774d69ff5c70e26b39b6fef6378";
+static const gchar *logind_dbus_sum      = "f698082a6b6c42aa0d9dbd8726adfe84ef08bb84779128fefc6bb40c9476b70b";
+static const gchar *logind_ispect_sum    = "557f98d71d1b4e62ecc5c7558db72f2293e42ee8ee9722ec3ffcffd9681a6bf3";
+static const gchar *timedated_dbus_sum   = "d7d67482a821cbc7a6ab3a68f1c00e0dc61a98c3c317461abe8b7aba7ac7b864";
+static const gchar *timedated_ispect_sum = "f576cb908636a285c9cd2e42a71ad243a0d5eb0ff12398f0627cce56c15a8268";
+
+#endif
+
+static gboolean set_xml_descriptors() {	
+
+}
+
+static gboolean populate_xml_data_dir() {
+
+}
+
+static gboolean read_xml_from_installconf() {
+
+}
