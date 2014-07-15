aa9bd64b241ef1ed52d227c1eaa530c046ce58cc
kremlin <ian@kremlin.cc>
2014-07-14 21:44:45 -0500

    get rid of ifdef maze in main.c, the binaries will be compiled sep.

--- src/main.c
+++ src/main.c
@@ -42,43 +42,28 @@ int main() {
 
 	GMainLoop *mloop;		
 	mloop = g_main_loop_new(NULL, TRUE);
+			
+	if(!systemd_utils_init()) {
+		g_printf("failed to init, are you root?\n");
+		return 1; /* TODO errno properly. grep for all "return 1;"s, not TODO'ing each one */
+	}
 
-	#ifdef COMPILE_HOSTNAMED_BINARY
-		hostnamed_init();
-	#endif
-	#ifdef COMPILE_LOCALED_BINARY
-		localed_init();
-	#endif
-	#ifdef COMPILE_TIMEDATED_BINARY
-	#endif
-	#ifdef COMPILE_LOGIND_BINARY
-	#endif
+	gboolean hostnamed_init_ok, localed_init_ok;
+	GPid *hostnamed_pid, *localed_pid;
+	gchar *hostnamed_argv[0], *localed_argv[0];
+	GSource *hostnamed_source, *localed_source, *timedated_source, *logind_source;
 
-	#if !defined(COMPILE_HOSTNAMED_BINARY) && !defined(COMPILE_LOCALED_BINARY) && !defined(COMPILE_TIMEDATED_BINARY) && !defined(COMPILE_LOGIND_BINARY)
-		
-		if(!systemd_utils_init()) {
-			g_printf("failed to init, are you root?\n");
-			return 1; /* TODO errno properly. grep for all "return 1;"s, not TODO'ing each one */
-		}
+	hostnamed_argv[0] = "/usr/local/libexec/systemd-hostnamed-handler";
+	localed_argv[0] = "/usr/local/libexec/systemd-localed-handler";
 
-		gboolean hostnamed_init_ok, localed_init_ok;
-		GPid *hostnamed_pid, *localed_pid;
-		gchar *hostnamed_argv[0], *localed_argv[0];
-		GSource *hostnamed_source, *localed_source, *timedated_source, *logind_source;
+	/*hostnamed_init_ok = g_spawn_async(NULL, hostnamed_argv, NULL, G_SPAWN_DEFAULT, NULL, NULL, hostnamed_pid, NULL);
+	localed_init_ok = g_spawn_async(NULL, localed_argv, NULL, G_SPAWN_DEFAULT, NULL, NULL, localed_pid, NULL);
 
-		hostnamed_argv[0] = "/usr/local/libexec/systemd-hostnamed-handler";
-		localed_argv[0] = "/usr/local/libexec/systemd-localed-handler";
+	hostnamed_source = g_child_watch_source_new(*hostnamed_pid);
+	localed_source = g_child_watch_source_new(*localed_pid); */
 
-		hostnamed_init_ok = g_spawn_async(NULL, hostnamed_argv, NULL, G_SPAWN_DEFAULT, NULL, NULL, hostnamed_pid, NULL);
-		localed_init_ok = g_spawn_async(NULL, localed_argv, NULL, G_SPAWN_DEFAULT, NULL, NULL, localed_pid, NULL);
-
-		hostnamed_source = g_child_watch_source_new(*hostnamed_pid);
-		localed_source = g_child_watch_source_new(*localed_pid);
-
-		/*g_source_attach(hostnamed_source, NULL);
-		g_source_attach(localed_source, NULL);*/
-
-	#endif
+	/*g_source_attach(hostnamed_source, NULL);
+	g_source_attach(localed_source, NULL);*/
 
 	g_main_loop_run(mloop);
 	g_main_loop_unref(mloop);
